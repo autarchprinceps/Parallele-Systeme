fd6106e008f71b1377cb02a37614604186302039
autarch princeps <autarch@outlook.com>
2014-10-27 13:20:59 +0100

    Ü2 A3 Erster Entwurf

--- assignments/Übung1/mpi_gather/ring.c
+++ assignments/Übung1/mpi_gather/ring.c
@@ -4,11 +4,17 @@
 
 int main(int argc, char **argv) {
 	int err, rank, n;
-
+	int* recbuf, sendbuf;
+	
 	err = MPI_Init(&argc, &argv);
 	err = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	err = MPI_Comm_size(MPI_COMM_WORLD, &n);	
-	// TODO
+	err = MPI_Comm_size(MPI_COMM_WORLD, &n);
+	sendbuf = malloc(sizeof(int));
+	*sendbuf = rank + 1;
+	if(rank == 0) {
+		recbuf = malloc(sizeof(int));
+	}
+	MPI_Gather(sendbuf, 1, MPI_INT, recbuf, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	err = MPI_Finalize();
 	return 0;
 }
