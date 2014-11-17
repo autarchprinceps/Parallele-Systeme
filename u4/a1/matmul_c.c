dd3e75757a41f113d40bc8056617084628321237
autarch princeps <autarch@outlook.com>
2014-11-17 14:55:11 +0100

    DEBUG

--- u4/a1/matmul_c.c
+++ u4/a1/matmul_c.c
@@ -49,8 +49,11 @@ static void matmul(int n, double a[n][n], double b[n][n], double c[n][n]) {
 		for(int jj = 0; jj < n; jj += B)
 			for(int i = 0; i < n; i++)
 				for(int k = kk; k < min(kk+B, n); k++) {
+					printf("%i %i %i %i ", kk, jj, i, k);
 					double tmp = b[i][k];
-					for(int j = jj; j < min(jj+B, n); j++) {	
+					printf("tmp done\n")
+					for(int j = jj; j < min(jj+B, n); j++) {
+						printf("%i", j);
 						a[i][j] += tmp * c[k][j];
 						/*a[i][j+1] += tmp * c[k][j+1];
 						a[i][j+2] += tmp * c[k][j+2];
@@ -60,7 +63,9 @@ static void matmul(int n, double a[n][n], double b[n][n], double c[n][n]) {
 						a[i][j+6] += tmp * c[k][j+6];
 						a[i][j+7] += tmp * c[k][j+7];
 						a[i][j+8] += tmp * c[k][j+8];*/
+						printf(".");
 					}
+					printf("\n");
 				}
 }
 
