e3dc174323cb75ba5411ef0eea0757b3c43b3611
autarch princeps <autarch@outlook.com>
2015-04-06 14:36:07 +0200

    -

--- paralg/u1/job_pp.sh
+++ paralg/u1/job_pp.sh
@@ -2,18 +2,19 @@
 #PBS -q mpi
 #PBS -l nodes=2:ppn=16
 #PBS -l walltime=00:30:00
-#PBS -l vmem=1GB
+#PBS -l vmem=16GB
 #PBS -N pp
 
 # change to job submit directory
 cd $PBS_O_WORKDIR
 
 # load openmpi module
-module load openmpi/gnu
+module load intel-icc
+module load openmpi/intel
 
 # determine number of available processors
 NNODES=`cat $PBS_NODEFILE | wc -l`
 
-mpirun -np $NNODES -machinefile $PBS_NODEFILE ./pp.exe
+mpirun -np $NNODES -machinefile $PBS_NODEFILE ./ping_pong.exe
 
 exit
--- u6/mandelbrot/job.sh
+++ u6/mandelbrot/job.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 #PBS -q mpi
 #PBS -l nodes=2:ppn=16
-#PBS -l walltime=00:30:00
+#PBS -l walltime=01:00:00
 #PBS -l vmem=16GB
 
 # change to job submit directory
 cd $PBS_O_WORKDIR
 
-module load openmpi/gnu
-module load metis/5.1.0-gcc-32
+module load intel-icc
+module load openmpi/intel
+module load metis/5.1.0-icc-64
 
 
 # determine number of MPI processes we have from the machinefile
 #NNODES=`cat $PBS_NODEFILE | wc -l`
 
 nprocs="2 4 8 16 32"
+#nprocs="8"
 
 # loop over all combinations of processor numbers
 for nd in $nprocs; do
