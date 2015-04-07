986b12face1f2b52e6a48280a7075cead096b516
autarch princeps <autarch@outlook.com>
2015-04-07 09:36:29 +0200

    FIX

--- paralg/u1/ping_pong.c
+++ paralg/u1/ping_pong.c
@@ -51,31 +51,45 @@ typedef struct {
 } resultset;
 void pp(unsigned int MSGLENSTART, unsigned int MSGLENMAX, unsigned int MSGLENINC, resultset* log) {
 	log->n = (MSGLENMAX - MSGLENSTART) / MSGLENINC;
-	char* buf = malloc(sizeof(char) * MSGLENMAX);
+	log->x = malloc(sizeof(double) * log->n);
+	log->y = malloc(sizeof(double) * log->n);
+	char* buf = calloc(sizeof(char), MSGLENMAX);
 	for(unsigned int i = 0; i < log->n; i++) {
 		unsigned int msglen = MSGLENSTART + i * MSGLENINC;
 		double tmin = 999999999;
+		// printf("DEBUG %d: outerloop %d %d\n", rank, i, msglen);
 		for(int iter = 0; iter < ITERMAX; iter++) {
+			// printf("DEBUG %d: prebarrier %d\n", rank, iter);
 			MPI_Barrier(MPI_COMM_WORLD);
+			// printf("DEBUG %d: postbarrier %d\n", rank, iter);
 			if(rank == 0) {
 				double t0 = gettime();
+				// printf("DEBUG %d: presend\n", rank);
 				MPI_Send(buf, msglen, MPI_BYTE, n-1, 0, MPI_COMM_WORLD);
 				MPI_Status status;
+				// printf("DEBUG %d: postsend\n", rank);
 				MPI_Recv(buf, msglen, MPI_BYTE, n-1, 0, MPI_COMM_WORLD, &status);
+				// printf("DEBUG %d: postrecv\n", rank);
 				t0 = (gettime() - t0) / 2;
 				if(t0 < tmin) tmin = t0;
-			} else {
+			} else if(rank == n-1) {
 				MPI_Status status;
+				// printf("DEBUG %d: prerecv\n", rank);
 				MPI_Recv(buf, msglen, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
+				// printf("DEBUG %d: postrecv\n", rank);
 				MPI_Send(buf, msglen, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+				// printf("DEBUG %d: postsend\n", rank);
 			}
 		}
 		if(rank == 0) {
+			// printf("DEBUG %d: prelog %d %f\n", rank, msglen, tmin);
 			log->x[i] = msglen;
 			log->y[i] = tmin;
+			// printf("DEBUG %d: postlog %d %f\n", rank, msglen, tmin);
 		}
 	}
 	if(rank == 0) {
+		// printf("DEBUG %d: prelsf\n", rank);
 		leastSquareFit(log->n, log->x, log->y, &log->alpha, &log->beta);
 	}
 }
@@ -96,16 +110,15 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &n);
 
-	if(rank == 0 || rank == n-1) {
-		resultset* log = malloc(sizeof(resultset));
-		pp(0, 16384, 256, log);
-		if(rank == 0) {
-			printRS("pp.0-2_14.csv", log);
-		}
-		pp(16384, 16777216, 261888, log);
-		if(rank == 0) {
-			printRS("pp.2_14-2_24.csv", log);
-		}
+	
+	resultset* log = malloc(sizeof(resultset));
+	pp(0, 16384, 256, log);
+	if(rank == 0) {
+		printRS("pp.0-2_14.csv", log);
+	}
+	pp(16384, 16777216, 261888, log);
+	if(rank == 0) {
+		printRS("pp.2_14-2_24.csv", log);
 	}
 
 	MPI_Finalize();
