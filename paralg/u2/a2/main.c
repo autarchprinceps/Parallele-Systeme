1ee247ec6ae9dd991e6ad7d6b56f320f308066e6
autarch princeps <autarch@outlook.com>
2015-04-28 18:08:45 +0200

    FIX

--- paralg/u2/a2/main.c
+++ paralg/u2/a2/main.c
@@ -7,7 +7,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <libFHBRS.h>
 #include "tree.h"
 
@@ -15,48 +14,48 @@ static atype_t sum(atype_t x, atype_t y) {
     return x + y;
 }
 
-atype_t test_seq(unsigned int n) {
-    atype_t values[n];
-    for(unsigned int i = 0; i < n; i++) {
+atype_t test_seq(unsigned long n) {
+    atype_t* values = malloc(n * sizeof(atype_t));
+    for(unsigned long i = 0; i < n; i++) {
         values[i] = i + 1;
     }
     double t0 = gettime();
     atype_t result = sum(values[0], values[1]);
-    for(unsigned int i = 2; i < n; i++) {
+    for(unsigned long i = 2; i < n; i++) {
         result = sum(result, values[i]);
     }
     t0 = gettime() - t0;
-    printf("seq: n: %u t: %e\n", n, t0);
+    printf("seq: n: %lu t: %e\n", n, t0);
+    free(values);
     return result;
 }
 
-atype_t test_par(unsigned int n, unsigned int p) {
-    unsigned int n2 = n - 1;
-    unsigned int n1 = n + n2;
-    atype_t values[n1];
-    for(unsigned int i = 0; i < n; i++) {
+atype_t test_par(unsigned long n, unsigned int p) {
+    unsigned long n2 = n - 1;
+    unsigned long n1 = n + n2;
+    atype_t* values = malloc(n1 * sizeof(atype_t));
+    for(unsigned long i = 0; i < n; i++) {
         values[n2 - 1 + i] = i + 1;
     }
     double t0 = gettime();
     atype_t result = balanced_tree(p, n1, values, &sum);
     t0 = gettime() - t0;
-    printf("par: n: %u p: %u t: %e\n",n, p, t0);
+    printf("par: n: %lu p: %u t: %e\n", n, p, t0);
+    free(values);
     return result;
 }
 
 int main(int argc, char** argv) {
-    unsigned int ns[] = {1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456};
+    unsigned long ns[] = {1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456};
     unsigned int ps[] = {1, 2, 4, 8, 16, 32};
     for(unsigned int i = 0; i < 9; i++) {
-	printf("\nn: %u\n", ns[i]);
+	printf("\nn: %lu\n", ns[i]);
         atype_t seq_res = test_seq(ns[i]);
-        // assert(seq_res == ns[i] * (ns[i] + 1) / 2);
-        printf("seq_res: %e should: %u\n", seq_res, ns[i] * (ns[i] + 1) / 2);
+        // printf("seq_res: %e should: %lu\n", seq_res, ns[i] * (ns[i] + 1) / 2);
         for(unsigned int j = 0; j < 6; j++) {
 	    printf("p: %u\n", ps[i]);
             atype_t par_res = test_par(ns[i], ps[j]);
-            // assert(par_res == seq_res);
-            printf("par_res: %e should: %u\n", par_res, ns[i] * (ns[i] + 1) / 2);
+            // printf("par_res: %e should: %lu\n", par_res, ns[i] * (ns[i] + 1) / 2);
         }
     }
     
--- paralg/u2/a2/tree.c
+++ paralg/u2/a2/tree.c
@@ -1,12 +1,12 @@
 #include "tree.h"
 
-atype_t balanced_tree(unsigned int p, unsigned int n1, atype_t values[n1], atype_t (*f)(atype_t x, atype_t y)) {
+atype_t balanced_tree(unsigned int p, unsigned long n1, atype_t values[n1], atype_t (*f)(atype_t x, atype_t y)) {
     unsigned int n = (n1 + 1) / 2;
-    for(unsigned int stride = n/2; stride > 0; stride /= 2) {
+    for(unsigned long stride = n/2; stride > 0; stride /= 2) {
         #pragma omp parallel num_threads(p)
         {
             #pragma omp for schedule(guided)
-            for(unsigned int i = 0; i < stride; i++) {
+            for(unsigned long i = 0; i < stride; i++) {
                 values[stride - 1 + i] = (*f)(values[stride*2-1 + i],values[stride*2-1 + i + stride]);
             }
         }
