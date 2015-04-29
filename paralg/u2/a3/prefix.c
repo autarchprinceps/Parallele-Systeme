cdcc21dab5f27e9303316a17dc1e98fd0c5c389a
autarch princeps <autarch@outlook.com>
2015-04-29 10:51:02 +0200

    -

--- paralg/u2/a3/prefix.c
+++ paralg/u2/a3/prefix.c
@@ -5,14 +5,14 @@ void prefix(unsigned int p, unsigned int n, atype_t values[n], atype_t (*f)(atyp
     atype_t last_elems[p-1];
     #pragma omp parallel num_threads(p)
     {
-        unsigned int rank;
-        unsigned int blockstart = n / p * rank; // TODO does rank start with 0? then correct else - 1;
+        unsigned int rank = omp_get_thread_num(); // [0 .. p-1]
+        unsigned int blockstart = n / p * rank; 
         unsigned int next_blockstart = n / p * (rank + 1); // TODO was wenn kann nicht ganz aufgeteilt werden
         for(unsigned int i = blockstart + 1; i < next_blockstart; i++) {
             values[i] = (*f)(values[i-1], values[i]);
         }
         #pragma omp barrier
-        if(rank == 0) { // or 1 ?
+        if(rank == 0) {
             unsigned int j = 0;
             last_elems[0] = values[n/p - 1];
             for(unsigned int i = n/p * 2 - 1; i < n; i += n/p) { // TODO letzter block nicht?
@@ -21,9 +21,9 @@ void prefix(unsigned int p, unsigned int n, atype_t values[n], atype_t (*f)(atyp
             }
         }
         #pragma omp barrier
-        if(rank > 0) { // or 1 ?
+        if(rank > 0) {
             for(unsigned int i = blockstart + 1; i < next_blockstart; i++) {
-                values[i] = (*f)(last_elems[rank - 1], values[i]); // or rank - 2?
+                values[i] = (*f)(last_elems[rank - 1], values[i]);
             }
         }
     }
