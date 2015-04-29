15c5a9ef864ef2d3a262360f0071226609fb657c
autarch princeps <autarch@outlook.com>
2015-04-29 14:00:05 +0200

    DEDEBUG

--- paralg/u2/a3/main.c
+++ paralg/u2/a3/main.c
@@ -15,8 +15,6 @@ static atype_t sum(atype_t x, atype_t y) {
     return x + y;
 }
 
-
-
 void test_seq(unsigned int n) {
     atype_t* values = malloc(sizeof(atype_t) * n);
     for(unsigned int i = 0; i < n; i++) {
@@ -27,12 +25,14 @@ void test_seq(unsigned int n) {
         values[i] = sum(values[i-1], values[i]);
     }
     t0 = gettime() - t0;
-    printf("seq: n: %u t: %e\n",n, t0);
+    printf("\nseq: n: %u t: %e\n",n, t0);
     if(n < 20) {
         for(unsigned int i = 0; i < n; i++) {
             printf("%f ", values[i]);
         }
         printf("\n");
+    } else {
+	printf("last: %e\n", values[n-1]);
     }
     free(values);
 }
@@ -51,20 +51,19 @@ void test_par(unsigned int n, unsigned int p) {
             printf("%f ", par_values[i]);
         }
         printf("\n");
+    } else {
+	printf("last: %e\n", par_values[n-1]);
     }
-    printf("pre free\n");
-    // free(par_values);
+    free(par_values); 
 }
 
 int main(int argc, char** argv) {
-    unsigned int ns[] = {8, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456};
+    unsigned int ns[] = {1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456};
     unsigned int ps[] = {1, 2, 4, 8, 16, 32, 38, 56};
-    for(unsigned int i = 0; i < 1/*9*/; i++) {
+    for(unsigned int i = 0; i < 9; i++) {
         test_seq(ns[i]);
-        for(unsigned int j = 0; j < 3/*8*/; j++) {
-	    printf("p: %d\n", ps[j]);
+        for(unsigned int j = 0; j < 8; j++) {
             test_par(ns[i], ps[j]);
-	    printf("done\n");
         }
     }
     
