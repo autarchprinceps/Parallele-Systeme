acab6caf6e5e2680956a3f499d0035d452c30da0
autarch princeps <autarch@outlook.com>
2015-06-24 12:02:37 +0200

    csv mark 1

--- paralg/u5/a3/main.c
+++ paralg/u5/a3/main.c
@@ -9,96 +9,174 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <libFHBRS.h>
 
-typedef double atype_t;
-
-atype_t seq_midpoint(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    atype_t h = (end - start) / n;
-    atype_t tmp = 0;
-    for(atype_t i = start; i < end; i += 2 * h) { // TODO Summe bis n/2 ?
-        tmp += 2 * h * (*f)(i);
+double seq_midpoint(double start, double end, unsigned int n, double (*f)(double)) {
+    double h = (end - start) / n;
+    double tmp = 0;
+    for(double i = start; i < end; i += 2 * h) {
+        tmp += (*f)(i);
     }
-    return tmp;
+    return 2 * h * tmp;
 }
 
-atype_t par_midpoint(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    atype_t h = (end - start) / n;
-    atype_t tmp = 0;
-    #pragma omp parallel for reduction(+:tmp)
-    for(atype_t i = start; i < end; i += 2 * h) { // TODO Summe bis n/2 ?
-        tmp += 2 * h * (*f)(i);
+double par_midpoint(unsigned int p, double start, double end, unsigned int n, double (*f)(double)) {
+    double h = (end - start) / n;
+    double tmp = 0;
+    #pragma omp parallel for reduction(+:tmp) num_threads(p)
+    for(unsigned int i = 0; i < n / 2; i++) {
+        double x = start + i * 2 * h;
+        tmp += (*f)(x);
     }
-    return tmp;
+    return 2 * h * tmp;
 }
 
-atype_t seq_trapezoid(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    atype_t h = (end - start) / n;
-    atype_t tmp = 0;
-    for(atype_t i = start; i < end; i += h) {
-        tmp += 0.5 * h * ((*f)(i) + (*f)(i + h));
+double seq_trapezoid(double start, double end, unsigned int n, double (*f)(double)) {
+    double h = (end - start) / n;
+    double tmp = 0;
+    for(double i = start; i < end; i += h) {
+        tmp += ((*f)(i) + (*f)(i + h));
     }
-    return tmp;
+    return 0.5 * h * tmp;
 }
 
-atype_t par_trapezoid(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    atype_t h = (end - start) / n;
-    atype_t tmp = 0;
-    #pragma omp parallel for reduction(+:tmp)
-    for(atype_t i = start; i < end; i += h) {
-        tmp += 0.5 * h * ((*f)(i) + (*f)(i + h));
+double par_trapezoid(unsigned int p, double start, double end, unsigned int n, double (*f)(double)) {
+    double h = (end - start) / n;
+    double tmp = 0;
+    #pragma omp parallel for reduction(+:tmp) num_threads(p)
+    for(unsigned int i = 0; i < n; i++) {
+        double x = start + i * h;
+        tmp += ((*f)(x) + (*f)(x + h));
     }
-    return tmp;
+    return 0.5 * h * tmp;
 }
 
-atype_t seq_simpson(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    atype_t h = (end - start) / n;
-    atype_t tmp = (*f)(start) + (*f)(end);
-    atype_t x = start + h;
+double seq_simpson(double start, double end, unsigned int n, double (*f)(double)) {
+    double h = (end - start) / n;
+    double tmp1 = 0;
+    double tmp2 = 0;
+    double x = start + h;
     for(unsigned int i = 0; i < n - 1; i++) {
         if(i % 2 == 0) {
-            tmp += 2 * (*f)(x);
+            tmp1 += (*f)(x);
         } else {
-            tmp += 4 * (*f)(x);
+            tmp2 += (*f)(x);
         }
         x += h;
     }
-    return tmp * h / 3;
+    return ((*f)(start) + (*f)(end) + 2 * tmp1 + 4 * tmp2) * h / 3;
 }
 
-atype_t par_simpson(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    atype_t h = (end - start) / n;
-    atype_t tmp = (*f)(start) + (*f)(end);
-    #pragma omp parallel for reduction(+:tmp)
+double par_simpson(unsigned int p, double start, double end, unsigned int n, double (*f)(double)) {
+    double h = (end - start) / n;
+    double tmp1 = 0;
+    double tmp2 = 0;
+    #pragma omp parallel for reduction(+:tmp1,tmp2) num_threads(p)
     for(unsigned int i = 0; i < n - 1; i++) {
-        atype_t x = start + (i + 1) * h;
+        double x = start + (i+1) * h;
         if(i % 2 == 0) {
-            tmp += 2 * (*f)(x);
+            tmp1 += (*f)(x);
         } else {
-            tmp += 4 * (*f)(x);
+            tmp2 += (*f)(x);
         }
     }
-    return tmp * h / 3;
+    return ((*f)(start) + (*f)(end) + 2 * tmp1 + 4 * tmp2) * h / 3;
 }
 
-atype_t seq_montecarlo(atype_t start, atype_t end, atype_t n, atype_t (*f)(atype_t)) {
-    return 0;
+double seq_montecarlo(double start, double end, unsigned int n, double (*f)(double)) {
+    void* generator = randp_init(0L);
+    double* r = (double *)malloc(n * sizeof(double));
+    randp_vector(generator, n, r);
+    double tmp = 0;
+    for(unsigned int i = 0; i < n; i++) {
+        double x = start + r[i] * (end - start);
+        tmp += (*f)(x);
+    }
+    return tmp * (end - start)/n;
 }
 
-/*
- * 
- */
+double par_montecarlo(unsigned int p, double start, double end, unsigned int n, double (*f)(double)) {
+    void* generator = randp_init(0L);
+    double* r = (double *)malloc(n * sizeof(double));
+    randp_vector(generator, n, r);
+    double tmp = 0;
+    #pragma omp parallel for reduction(+:tmp) num_threads(p)
+    for(unsigned int i = 0; i < n; i++) {
+        double x = start + r[i] * (end - start);
+        tmp += (*f)(x);
+    }
+    return tmp * (end - start)/n;
+}
+
 int main(int argc, char** argv) {
-    atype_t b = 10000;
-    atype_t a = -1000;
-    atype_t n = 123456789;
-    printf("Exact: %e\n", -cos(b) + cos(a)); // F(x) = -cos(x)
-    printf("Seq Midpoint: %e\n", seq_midpoint(a, b, n, sin));
-    printf("Par Midpoint: %e\n", par_midpoint(a, b, n, sin));
-    printf("Seq Trapezoid: %e\n", seq_trapezoid(a, b, n, sin));
-    printf("Par Trapezoid: %e\n", par_trapezoid(a, b, n, sin));
-    printf("Seq Simpson: %e\n", seq_simpson(a, b, n, sin));
-    printf("Par Simpson: %e\n", par_simpson(a, b, n, sin));
-    printf("Seq MonteCarlo: %e\n", seq_montecarlo(a, b, n, sin));
+    double b = 1000;
+    double a = 0;
+    unsigned int ps[] = {1,2,4,8,16,24,32,48,56};
+    
+    double exactResult = -cos(b) + cos(a); // F(x) = -cos(x)
+    printf("exact;%e;;;;;;;;;;;;;;;;;;;\n", exactResult);
+    printf("n;seq;;par;;;;;;;;;;;;;;;;;\n");
+    printf(";;");
+    for(unsigned int pidx = 0; pidx < 9; pidx++) {
+        printf(";%u;", ps[pidx]);
+    }
+    printf("\n");
+    for(unsigned int pidx = 0; pidx < 9; pidx++) {
+        printf(";time;result");
+    }
+    printf("\nmidpoint\n");
+    for(unsigned int n = 1; n <= 1000000; n *= 10) {
+        double t, result;
+        t = gettime();
+        result = seq_midpoint(a, b, n, sin);
+        t = gettime() - t;
+        printf("%u;%e;%e", n, t, result);
+        for(unsigned int pidx = 0; pidx < 9; pidx++) {
+            t = gettime();
+            result = par_midpoint(ps[pidx], a, b, n, sin);
+            t = gettime() - t;
+        }
+    }
+    printf("\ntrapezoid\n");
+    for(unsigned int n = 1; n <= 1000000; n *= 10) {
+        double t, result;
+        t = gettime();
+        result = seq_trapezoid(a, b, n, sin);
+        t = gettime() - t;
+        printf("%u;%e;%e", n, t, result);
+        for(unsigned int pidx = 0; pidx < 9; pidx++) {
+            t = gettime();
+            result = par_trapezoid(ps[pidx], a, b, n, sin);
+            t = gettime() - t;
+        }
+    }
+    printf("\nsimpson\n");
+    for(unsigned int n = 1; n <= 1000000; n *= 10) {
+        double t, result;
+        t = gettime();
+        result = seq_simpson(a, b, n, sin);
+        t = gettime() - t;
+        printf("%u;%e;%e", n, t, result);
+        for(unsigned int pidx = 0; pidx < 9; pidx++) {
+            t = gettime();
+            result = par_simpson(ps[pidx], a, b, n, sin);
+            t = gettime() - t;
+        }
+    }
+    printf("\nmontecarlo\n");
+    for(unsigned int n = 1; n <= 1000000; n *= 10) {
+        double t, result;
+        t = gettime();
+        result = seq_montecarlo(a, b, n, sin);
+        t = gettime() - t;
+        printf("%u;%e;%e", n, t, result);
+        for(unsigned int pidx = 0; pidx < 9; pidx++) {
+            t = gettime();
+            result = par_montecarlo(ps[pidx], a, b, n, sin);
+            t = gettime() - t;
+        }
+    }
+    printf("\n\n");
 
     return (EXIT_SUCCESS);
 }
